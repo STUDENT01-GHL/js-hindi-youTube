ee3f3b77b5d0adf71cce596fad853a8a932ba2f0
STUDENT01-GHL <suhanigohel1022k15@gmail.com>
2025-05-13 05:46:03 +0000

    conversion of types

--- 01basics/03conversionOperation.js
+++ 01basics/03conversionOperation.js
@@ -30,5 +30,61 @@ console.log(typeof stringNumber);
 
 
 
+//    Operations   //
+
+
+let number = 5
+let negValue = -number
+console.log(negValue);
+
+
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3**3);
+console.log(3/3);
+console.log(3%3);
+
+
+let str1 = "hello"
+let str2 = " Suhani"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log(2 + "4");
+console.log("2" + 4);
+console.log("1" + 2 + 2 + "2");
+
+
+console.log(+"");
+console.log(+true);
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+let a = 5
+const b = a++
+
+console.log(`a:${a} , b:${b}`);
+
+let x = 11;
+const y = ++x
+
+console.log(`x:${x} , y:${y}`);
+
+
+
+
+
+
+
+
+
+
+
 
 
