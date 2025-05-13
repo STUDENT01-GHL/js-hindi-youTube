7a9408f523899d78acd170007c1da5ae54c35443
STUDENT01-GHL <suhanigohel1022k15@gmail.com>
2025-05-13 05:12:25 +0000

    conversion of datatypes

---
+++ 01basics/03conversionOperation.js
@@ -0,0 +1,34 @@
+let score = false
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);   // it will be not a number
+
+
+// "33" => 33
+// "33abc" => NaN => not a number 
+// true =>1 ; false =>0 
+
+
+let isLoggedIn = "Suhu"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+// 1 => true ; 0 => false
+
+
+
+let someNumber = 33
+let stringNumber =  String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
