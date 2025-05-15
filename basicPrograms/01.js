d0b3b8646c4fbfff8018ac013890098e8eb5c501
STUDENT01-GHL <suhanigohel1022k15@gmail.com>
2025-05-15 16:38:16 +0000

    basic programs

---
+++ basicPrograms/01.js
@@ -0,0 +1,9 @@
+// variables
+
+var state = "Gujrat"
+let country = "India"
+const id = "001"
+
+console.log(state);
+console.log(country);
+console.log(id);
---
+++ basicPrograms/02.js
@@ -0,0 +1,18 @@
+// datatypes
+
+// string , number  , boolean  , bigInt , null , undefined
+
+let address = "Ghatlodia"
+let rollno = 21
+let isActive = true
+let bigNum = 763547637n
+let score = null    // object
+let schedule 
+
+console.log(typeof address);
+console.log(typeof rollno);
+console.log(typeof isActive);
+console.log(typeof bigNum);
+console.log(typeof score);
+console.log(typeof schedule);
+console.log(typeof address);
