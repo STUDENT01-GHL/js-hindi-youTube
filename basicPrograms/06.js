4b07d084e81f3ef1e11b2c83cac3e8926238f502
STUDENT01-GHL <suhanigohel1022k15@gmail.com>
2025-05-15 17:11:52 +0000

    basic

---
+++ basicPrograms/06.js
@@ -0,0 +1,15 @@
+// loops
+
+for (let i = 10; i <= 1000; i++) {
+    //const element = array[i];
+  console.log("for loop:" , i);
+}
+
+// while loop
+
+let i = 5;
+while (i <= 40) {
+    console.log("while loop:" , i);
+    i++;
+    
+}
---
+++ basicPrograms/07.js
@@ -0,0 +1,8 @@
+let colors = ["red", "green", "blue"];
+console.log(colors[1]); // green
+
+let person = {
+  name: "Neha",
+  age: 22
+};
+console.log(person.name); // Neha
