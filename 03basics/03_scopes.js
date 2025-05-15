9025d22930449d8c3a22c83ca30d3c07e7f5efda
STUDENT01-GHL <suhanigohel1022k15@gmail.com>
2025-05-15 07:26:55 +0000

    scope 2

--- 03basics/03_scopes.js
+++ 03basics/03_scopes.js
@@ -11,6 +11,53 @@ if (true) {
 
 
 console.log(a);
-//console.log(b);
+console.log(b);
 console.log(c);
+
+function one(){
+    const username = "Suhani"
+
+    function two(){
+        const website = "YouTube"
+        console.log(username);
+    }
+    //console.log(website);
+   
+    // two()
+}
+
+// one()
+
+if (true) {
+    const username = "Suru"
+
+    if(username === "Suru") {
+        const website = "  Utube"
+        console.log(username + website);
+        
+    }
+
+    console.log(website);
+    
+}
+
+console.log(username);
+
+// ----- interesting -------
+
+console.log(addOne(5));
+
+
+function addOne(num) {
+    return num + 1
+}
+
+//console.log(addTwo(7))
+
+const addTwo = function(num) {
+    return num + 3
+}
+
+// console.log(addTwo(7))
+
 
