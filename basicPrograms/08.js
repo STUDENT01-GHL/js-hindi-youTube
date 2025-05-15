8845278597f6264936d68c20c917965539e9b3a6
STUDENT01-GHL <suhanigohel1022k15@gmail.com>
2025-05-15 17:18:13 +0000

    basic

---
+++ basicPrograms/08.js
@@ -0,0 +1,20 @@
+// scope
+
+let global = "Global";
+
+
+function showGlobal(){
+    console.log(global);
+    
+}
+
+showGlobal();
+
+// function 
+
+function test(){
+    let fun = "Function scope"
+    console.log(fun);
+    
+}
+test();
