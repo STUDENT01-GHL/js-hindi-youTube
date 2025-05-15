6a9f064a2430212ca2ce03d1a63440d7eeb4f6e9
STUDENT01-GHL <suhanigohel1022k15@gmail.com>
2025-05-15 16:54:15 +0000

    basic

---
+++ basicPrograms/04.js
@@ -0,0 +1,15 @@
+let score = 200
+
+if(score >= 210)
+{
+    console.log("Can be win");
+}
+else if(score >= 180) 
+    {
+        console.log("50-50 % chance to win");
+    }
+    else
+    {
+        console.log("Can't say");
+        
+    }
---
+++ basicPrograms/05.js
@@ -0,0 +1,25 @@
+// functions
+  
+// function declaration 
+
+
+function intro(details) {
+    return "Heyy , " +  details
+}
+
+console.log(intro("Here is Mehndi details"));
+
+
+// function expression
+
+const cube = function(s) {
+    return s * s * s;
+};
+
+console.log(cube(21));
+
+// arrow function
+
+const join = (str1 , str2) => str1 + str2 ; 
+console.log(join("suhani" , " Gohel"));
+
