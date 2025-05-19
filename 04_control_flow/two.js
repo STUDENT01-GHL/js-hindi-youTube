71446d638031ce6ddeb155e20d67ed74368f0c91
STUDENT01-GHL <suhanigohel1022k15@gmail.com>
2025-05-19 05:33:17 +0000

    switch statement

---
+++ 04_control_flow/two.js
@@ -0,0 +1,18 @@
+const birthYear =  2005
+
+switch(birthYear) {
+    case 2000:
+    console.log("Born in 2000");
+    break;
+    case 2001:
+    console.log("Born in 2001");
+    break;
+    case 2005:
+    console.log("Born in 2005");
+    break;
+    case 2009:
+    console.log("Born in 2009");
+    break;
+    default :
+    console.log(" year not specified");
+}
