4a39b3f2143342bd5543c80e9c2c7c685ba0ca53
Suhani <suhanigohel1022k15@gmail.com>
2025-05-26 09:18:00 +0000

    basic of asychronous

---
+++ asynchronous/1.js
@@ -0,0 +1,25 @@
+// function myFirst() {
+//     myDisplayer("Hi");
+// }
+
+// function mySecond() 
+// {
+//       myDisplayer("Hello");
+// }
+
+// myFirst();
+// mySecond();
+
+function myFirst() {
+ console.log("Hi");
+}
+
+function mySecond() 
+{
+       console.log("Hello");
+}
+
+
+mySecond();
+myFirst();
+
